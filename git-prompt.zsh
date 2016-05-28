504de01bfb3921c5e0aeab28960d4f739002b227
Chris J. Lebron <chris@chrisjlebron.com>
2016-05-28 18:21:03 -0400

    Make customizations for git prompt to work with pure

--- git-prompt.zsh
+++ git-prompt.zsh
@@ -15,9 +15,10 @@ GIT_PROMPT_AHEAD="%{$fg[red]%}ANUM%{$reset_color%}"
 GIT_PROMPT_BEHIND="%{$fg[cyan]%}BNUM%{$reset_color%}"
 GIT_PROMPT_MERGING="%{$fg_bold[magenta]%}⚡︎%{$reset_color%}"
 GIT_PROMPT_REBASE="%{$fg_bold[magenta]%}®%{$reset_color%}"
-GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}●%{$reset_color%}"
-GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}●%{$reset_color%}"
-GIT_PROMPT_STAGED="%{$fg_bold[green]%}●%{$reset_color%}"
+GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}?%{$reset_color%}"
+GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}!%{$reset_color%}"
+GIT_PROMPT_STAGED="%{$fg_bold[green]%}+%{$reset_color%}"
+GIT_PROMPT_STASHED="%{$fg_bold[green]%}$%{$reset_color%}"
 
 # Show Git branch/tag, or name-rev if on detached head
 parse_git_branch() {
@@ -31,25 +32,31 @@ parse_git_state() {
   local GIT_STATE=""
   local GIT_STATUS="$(git status --branch --porcelain)"
 
-  local NUM_AHEAD="$(echo "$GIT_STATUS" | head -1 |
-    grep 'ahead [0-9]*' | sed -e 's/.*ahead \([0-9]*\).*/\1/')"
-  if [[ $NUM_AHEAD -gt 0 ]]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
-  fi
+  # If combining with Pure, their config for some bits, which is more explicit
+  # about merge/rebase/cherry-pick, and less about number of commits ahead/behind
+  if [[ !${USE_PURE_GIT_PROMPT:-false} ]]; then
 
-  local NUM_BEHIND="$(echo "$GIT_STATUS" | head -1 |
-    grep 'behind [0-9]*' | sed -e 's/.*behind \([0-9]*\).*/\1/')"
-  if [[ $NUM_BEHIND -gt 0 ]]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
-  fi
+    local NUM_AHEAD="$(echo "$GIT_STATUS" | head -1 |
+      grep 'ahead [0-9]*' | sed -e 's/.*ahead \([0-9]*\).*/\1/')"
+    if [[ $NUM_AHEAD -gt 0 ]]; then
+      GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
+    fi
 
-  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-  if [[ -n $GIT_DIR ]] && [[ -r $GIT_DIR/MERGE_HEAD ]]; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
-  fi
+    local NUM_BEHIND="$(echo "$GIT_STATUS" | head -1 |
+      grep 'behind [0-9]*' | sed -e 's/.*behind \([0-9]*\).*/\1/')"
+    if [[ $NUM_BEHIND -gt 0 ]]; then
+      GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
+    fi
 
-  if [[ -n $GIT_DIR ]] && [[ -d $GIT_DIR/rebase-apply ]] || [[ -d $GIT_DIR/rebase-merge ]] then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_REBASE
+    local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+    if [[ -n $GIT_DIR ]] && [[ -r $GIT_DIR/MERGE_HEAD ]]; then
+      GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
+    fi
+
+    if [[ -n $GIT_DIR ]] && [[ -d $GIT_DIR/rebase-apply ]] || [[ -d $GIT_DIR/rebase-merge ]] then
+      GIT_STATE=$GIT_STATE$GIT_PROMPT_REBASE
+    fi
+
   fi
 
   if echo "$GIT_STATUS" | grep -q '^??'; then
@@ -62,6 +69,10 @@ parse_git_state() {
 
   if echo "$GIT_STATUS" | grep -q '^[ACDMR]'; then
     GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
+  fi
+
+  if $(git rev-parse --verify refs/stash &>/dev/null); then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_STASHED
   fi
 
   if [[ -n $GIT_STATE ]]; then
